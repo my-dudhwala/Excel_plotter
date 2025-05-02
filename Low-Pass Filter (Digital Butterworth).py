cfd221fd6e0d5217b3ee53eabf2e7857e1617510
my-dudhwala <shaikhmohammedyasar7@gmail.com>
2025-05-02 15:59:48 +0530

    Added Arduino code

--- Low-Pass Filter (Digital Butterworth).py
+++ Low-Pass Filter (Digital Butterworth).py
@@ -10,19 +10,19 @@ df = pd.read_excel(file_path)
 data = df.iloc[:, 0].dropna().reset_index(drop=True)
 
 def butter_lowpass_filter(data, cutoff, fs, order=4):
-    nyq = 0.5 * fs
+    nyq = 0.4 * fs
     normal_cutoff = cutoff / nyq
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
     y = filtfilt(b, a, data)
     return y
 
 # Parameters (adjust as needed)
-fs = 150  # Sampling frequency in Hz
+fs = 450  # Sampling frequency in Hz
 cutoff = 20  # Desired cutoff frequency of the filter, in Hz
 filtered = butter_lowpass_filter(data, cutoff, fs)
 
 plt.figure(figsize=(10, 5))
-plt.plot(data, alpha=0.5, label='Original', linestyle='--')
+plt.plot(data, alpha=0.2, label='Original', linestyle='--')
 plt.plot(filtered, label='Butterworth Filtered', color='purple')
 plt.title('Smoothed Arduino Data (Low-pass Filter)')
 plt.xlabel('Sample Number')
